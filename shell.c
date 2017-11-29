a2c6f3c3b6454a89f4fbb027b99f907454ad82bb
Kyle Lin <klin12@stuy.edu>
2017-11-29 14:57:46 -0500

    Initial Commit

---
+++ shell.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+
+int main() {
+    while (1) {
+        printf(">");
+        bash_shell();
+    }
+    return 0;
+}
---
+++ shell.h
@@ -0,0 +1,9 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "./parser/parser.h"
+#include "./bash_shell/bash_shell.h"
+#include "./exec_commands/exec_commands.h"
+#endif
