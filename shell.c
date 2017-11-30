f61258be23f44546e1ae834d88d79de91bdafe21
Kyle Lin <klin12@stuy.edu>
2017-11-30 07:52:30 -0500

    .

--- shell.c
+++
@@ -1,9 +0,0 @@
-#include "shell.h"
-
-int main() {
-    while (1) {
-        printf(">");
-        bash_shell();
-    }
-    return 0;
-}
--- shell.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHELL_H
-#define SHELL_H
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "./parser/parser.h"
-#include "./bash_shell/bash_shell.h"
-#include "./exec_commands/exec_commands.h"
-#endif
